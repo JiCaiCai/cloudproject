bee36af57ab875090709f01904ced571b2d21fd4
zuliang <hduser@ubuntu.(none)>
2014-04-09 20:12:16 +0800

    mirror change for photo class

---
+++ src/com/facehandsome/bean/Photo.java
@@ -0,0 +1,26 @@
+package com.facehandsome.bean;
+
+public class Photo {
+	private String name;
+	private String path;
+	private String handsome;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getHandsome() {
+		return handsome;
+	}
+	public void setHandsome(String handsome) {
+		this.handsome = handsome;
+	}
+}
